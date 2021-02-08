f9d2b0f08d1b18f1bfb7f970fb0531636da4fef5
Lee-Yeonghyeon <yeonghyeon0403@naver.com>
2021-02-08 21:59:07 +0900

    1) UI 파일 오류 수정 2) 로그인 및 회원가입 기능 구현 3) UI 디자인 업로드

--- app/src/main/java/com/example/portfolioapp/PortfolioViewActivity.kt
+++ app/src/main/java/com/example/portfolioapp/PortfolioViewActivity.kt
@@ -11,9 +11,4 @@ class PortfolioViewActivity : AppCompatActivity() {
 
         supportActionBar?.setTitle("ㅇㅇ 공모전") //사용자가 선택한 공모전 이름이 타이틀로 바뀔 수 있게 수정해주세요
     }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean { //사용자가 선택한 공모전의 dailyPortfolio로 이동할 수 있도록 연결해주세요(액션바에 있는 아이콘임)
-        menuInflater.inflate(R.menu.fordaily, menu)
-        return true
-    }
 }
