e3e764309fb2f899959c781f707cef18b27aebe1
seeyeon <spring8024@naver.com>
2021-02-11 23:12:10 +0900

    시험작,,

--- app/src/main/java/com/example/portfolioapp/ItemPort.kt
+++ app/src/main/java/com/example/portfolioapp/ItemPort.kt
@@ -1,4 +1,11 @@
 package com.example.portfolioapp
 
 class ItemPort(val port_title: String, val port_startdate: String, val port_enddate: String, val port_content: String) {
+
+
+
+    //안녕?
+    //우리 왜
+    //우리 조금만 더 힘내요!!
+
 }
